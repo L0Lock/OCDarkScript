57814c0b11644f8f5eaac6c34c04a9109855be4e
DAUTRY Loïc <16049822+L0Lock@users.noreply.github.com>
2018-06-18 16:29:20 +0200

    Create ocdarkscript.js

---
+++ ocdarkscript.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name			OCDark
+// @namespace		https://github.com/L0Lock/OCDarkScript
+// @updateURL		https://raw.githubusercontent.com/L0Lock/OCDarkScript/master/ocdarkscript.js
+// @downloadURL		https://raw.githubusercontent.com/L0Lock/OCDarkScript/master/ocdarkscript.js
+// @version			0.1
+// @description		try to take over the world!
+// @author			-L0Lock-
+// @match			https://openclassrooms.com/*
+// @grant			none
+// ==/UserScript==
+
+(function() {
+	'use strict';
+
+	var cssId = 'myCss'; // you could encode the css path itself to generate id..
+	if (!document.getElementById(cssId))
+	{
+		var head = document.getElementsByTagName('head')[0];
+		var link = document.createElement('link');
+		link.id = cssId;
+		link.rel = 'stylesheet';
+		link.type = 'text/css';
+		link.href = 'https://raw.githubusercontent.com/L0Lock/OCDarkScript/master/ocdark.css';
+		link.media = 'all';
+		head.appendChild(link);
+	}
+})();
